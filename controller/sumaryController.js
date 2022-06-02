0cfdffc7c35d3f06d7bc2c29c305db38848a3e98
Carlos Sierra <carlossierra850@gmail.com>
2022-06-02 00:13:29 -0400

    testing cahnges of cicles.2

--- controller/sumaryController.js
+++ controller/sumaryController.js
@@ -2,6 +2,7 @@ const moment = require('moment')
 const { calculeRevenues } = require('../helpers')
 const Companies = require('../model/Companies')
 const Requests = require('../model/Requests')
+const Summaries = require('../model/Summaries')
 exports.getSumaryCompany = async (req, res) => {
   const company = req.company.id
   const { daysRange } = req.query
@@ -14,7 +15,21 @@ exports.getSumaryCompany = async (req, res) => {
     }
     const requestLastMonth = filterByMonthAgo()
     const earnings = calculeRevenues({ arr: requestLastMonth })
-    res.status(200).json({ earnings, orderLength: requestLastMonth.length })
+    res.status(200).json({
+      earnings,
+      orderLength: requestLastMonth.length,
+      timeRange: { startDate, endDate: dateRef },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+exports.getSumaryHistory = async (req, res) => {
+  const company = req.company.id
+  try {
+    const histories = await Summaries.find({ company }).sort({ date: -1 })
+    console.log(histories)
+    res.status(200).json(histories)
   } catch (error) {
     console.log(error)
   }
--- routes/summary.js
+++ routes/summary.js
@@ -4,4 +4,5 @@ const auth = require('../middleware/auth')
 const setDateRange = require('../middleware/setDateRange')
 const router = express.Router()
 router.get('/', auth, setDateRange, summaryController.getSumaryCompany)
+router.get('/history', auth, setDateRange, summaryController.getSumaryHistory)
 module.exports = router
