d072cf10cd24f0ba637e30b35f31c3c8d66f2d72
Carlos Sierra <carlossierra850@gmail.com>
2022-10-31 22:47:25 -0400

    deploy test

--- socket/index.js
+++ socket/index.js
@@ -6,7 +6,9 @@ const socketIo = require('socket.io')
 const { editNotification } = require('./utils/NotificationsSocket')
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: (process.env.NODE_ENV = 'production'
+      ? process.env.CLIENT_URL
+      : 'http://localhost:3000'),
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 })
