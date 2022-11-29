6713b8e5de9aa989e53c7f9f5f2b30df1d7fb774
Carlos Sierra <carlossierra850@gmail.com>
2022-11-29 16:37:30 -0400

    fix: conflicts merge aaa

--- controller/authController.js
+++ controller/authController.js
@@ -57,7 +57,8 @@ exports.sendToken = async (req, res) => {
     expiresIn: 3600,
   })
   company.tokenResetPass = token
-  console.log(`${FRONTEND_URL}/reset-password/${token}`)
+  const link = `${FRONTEND_URL}/reset-password/${token}`
+  console.log(link)
   company.save()
   return res.status(200).json({
     msg: `Enter your email address to send a password reset link ${company.companyEmail}`,
