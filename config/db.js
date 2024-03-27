a84810c0e3d963eb5ad8286815523f99aecb3ff4
Carlos Sierra <carlossierra850@gmail.com>
2024-03-27 02:21:28 -0400

    moving the hands to deploy

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config({ path: '.env' })
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const conectarDB = async () => {
   try {
     await mongoose.connect(
-      process.env.NODE_ENV === 'production'
-        ? process.env.DB_URL
-        : process.env.DB_LOCAL
+     "mongodb+srv://carlos:carlos123@cluster0.7j2xq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", clientOptions
     )
     console.log('DB connected', process.env.DB_URL,  process.env.NODE_ENV)
   } catch (error) {
