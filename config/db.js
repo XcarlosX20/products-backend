8551016d9b06ef7c0a64c17bf3f6211faafea1a2
Carlos Sierra <carlossierra850@gmail.com>
2024-03-27 02:42:38 -0400

    moving the hands to deploy

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const clientOptions = { serverApi: { version: '1', strict: true, deprecationErro
 const conectarDB = async () => {
   try {
     await mongoose.connect(
-     "mongodb+srv://carlos:carlos123@cluster0.7j2xq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", clientOptions
+     "mongodb+srv://carlos:carlos123@cluster0.7j2xq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&appName=Cluster0", clientOptions
     )
     console.log('DB connected', process.env.DB_URL,  process.env.NODE_ENV)
   } catch (error) {
